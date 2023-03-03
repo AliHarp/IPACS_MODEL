60b693c31671f62a2dc773a2d32a7de082ebed2d
amyheather <a.heather2@exeter.ac.uk>
2023-03-03 17:10:49 +0000

    Updated following bug fix in P1 model intialisation

--- testing/check_for_changes.R
+++ testing/check_for_changes.R
@@ -4,9 +4,12 @@ library(lintr)
 
 # Looks for differences between latest visit output, and output from 
 # original version of code
-visit_data_new <- read.csv(here("outputs", "visit_output_using_IPACS_20230214_fix.csv"))
-feb_14 <- read.csv(here("testing", "Scenarios_Visit_based_output_2023-02-14.csv"))
-diffdf::diffdf(visit_data_new, feb_14)
+visit_new <- read.csv(here("outputs", "visit_output_using_IPACS_20230214_fix.csv"))
+visit_test <- read.csv(here("testing", "visit_testing_using_IPACS_20230214_fix.csv"))
+diffdf::diffdf(visit_new, visit_test)
+
+# No longer check using copy from original code, due to bug fix
+# Hence renamed visit_testing_prior_to_bug_fix_for_IPACS_20230214_fix.csv
 
 # Linting
 lint("IPACS_model_script.R")
