668500d76fdcc60702e46f5fb6707b23ca9d3e2c
amyheather <a.heather2@exeter.ac.uk>
2023-03-09 16:44:44 +0000

    Add patients as function for all three parts of simulation

--- functions/visit_functions.R
+++ functions/visit_functions.R
@@ -144,42 +144,44 @@ create_summary_df <- function(nrow){
   return(df)
 }
 
-check_resources <- function(df){
-  # Planning service, check resources
-  # Create temporary t for incrementing when no resources available
-  tt <- t
-  # Create adjusted LOS
-  los_adj <- df$los[npat] - 1
-  # While start_service = NA
-  while (is.na(df$start_service[npat]) == TRUE) {
-    # If resources columns (from tt to LOS-1) are >= req_visits
-    if (all(resources[tt:(tt + los_adj), ] >= req_visits[[id]]) == TRUE) {
-      df$start_service[npat] <- tt
-      df$end_service[npat] <- tt + los_adj
-      # Decrease capacity
-      resources[tt:(tt + los_adj), ] <- resources[tt:(tt + los_adj), ] - req_visits[[id]]
-    } else {
-      # If no sufficient resources, check for starting on the next day
-      tt <- tt + 1
-    }
-  }
-  return(list(df, resources))
-}
 
-
-add_patient <- function(){
+add_patient <- function(in_system){
+  # in_system - are they already in system?
+  
   # Increment ID and npat
   id <- id + 1
   npat <- npat + 1
   
-  # Find LOS and required visits
-  los <- dis_los()
-  init_slots <- dis_init_slots()
-  end_slots <- dis_end_slots()
-  visit_vector <-
-    round(seq(from = init_slots,
-              to = end_slots,
-              length.out = los)) #full visit seq
+  if (in_system == FALSE) {
+    # Find LOS and required visits
+    los <- dis_los()
+    init_slots <- dis_init_slots()
+    end_slots <- dis_end_slots()
+    visit_vector <-
+      round(seq(from = init_slots,
+                to = end_slots,
+                length.out = los)) #full visit seq
+  } else if (in_system == TRUE) {
+    # Create temporary LOS using dis_los() (so it is longer)
+    # Then get shorter LOS using dis_los2() which trims the templos
+    # Then get end_slots (final number of visits) and init_slots (initial
+    # number). Create sequence, then sample from tail for length of the
+    # shorter LOS. This means patients already in system have a shorter LOS
+    # and start from a later point that init_slots.
+    # Then save vector of required visits
+    # e.g.
+    # temp vector: 4 4 4 3 3 3 3 2 2 2 2 2 1 1 1
+    # final vector: 2 1 1 1
+    templos <- dis_los()
+    los <- dis_los2(templos)
+    init_slots <- dis_init_slots()
+    end_slots <- dis_end_slots()
+    temp_visit_vector <- round(seq(from = init_slots,
+                                   to = end_slots,
+                                   length.out = templos))
+    visit_vector <- tail(temp_visit_vector, los)
+  }
+  
   req_visits[[id]] <- visit_vector
   
   # Save to patients_inqueue dataframe
@@ -214,17 +216,3 @@ add_patient <- function(){
   # or dataframes we use elsewhere)
   return(list(id, npat, req_visits, patients, resources))
 }
-
-# # Save information to patients dataframe
-# save_patient_info <- function(df, npat, id, los, arrival_time,
-#                               start_service, end_service, wait_time,
-#                               exit){
-#   df$id[npat] <- id
-#   df$los[npat] <- los
-#   df$arrival_time[npat] <- arrival_time
-#   df$start_service[npat] <- start_service
-#   df$end_service[npat] <- end_service
-#   df$wait_time[npat] <- wait_time
-#   df$exit[npat] <- exit
-#   return(df)
-# }
