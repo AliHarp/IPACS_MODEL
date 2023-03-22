777a05d4240548252d282045274a09a7e2bf9102
amyheather <a.heather2@exeter.ac.uk>
2023-03-22 16:30:16 +0000

    Added lint of bed functions, and a rudimentary check of median and mean (to be improved)

--- testing/testing_and_linting.R
+++ testing/testing_and_linting.R
@@ -2,6 +2,11 @@ library(diffdf)
 library(here)
 library(lintr)
 
+# Clear workspace and set working directory
+rm(list = ls())
+wd <- setwd("~/Documents/IPACS_MODEL")
+
+# Checking results are consistent ----------------------------------------------
 # Looks for differences between latest visit output, and output from 
 # original version of code
 visit_new <- read.csv(here("outputs", "visit_output_using_IPACS_20230214_fix.csv"))
@@ -13,10 +18,28 @@ bed_new <- read.csv(here("outputs", "bed_output_using_IPACS_20230214_fix.csv"))
 bed_test <- read.csv(here("testing", "bed_testing_using_IPACS_20230214_fix.csv"))
 diffdf::diffdf(bed_new, bed_test)
 
-# Linting
+# Linting ---------------------------------------------------------------------
 lint("ipacs_main_script.R")
 lint(here("functions", "set_up.R"))
 lint(here("functions", "visit_functions.R"))
 lint(here("functions", "visit_model.R"))
+lint(here("functions", "bed_functions.R"))
 lint(here("functions", "bed_model.R"))
 lint("ipacs_produce_report.Rmd")
+
+# Testing ---------------------------------------------------------------------
+# Name of file with model parameters (should be stored in model_inputs folder)
+input_filename <- "IPACS_20230214_fix.xlsx"
+input_list <- list(c("arrivals_all", "arrivals"),
+                   c("init_conds", "initial conditions"),
+                   c("capacity", "capacity"),
+                   c("losA", "los"),
+                   c("costs", "costs"))
+for (x in input_list){
+  assign(x[1], readxl::read_excel(here("model_inputs", input_filename),
+                                  sheet = x[2]))
+}
+
+# AMY: Ideally need to be able to call to run those rather than rewrite code here
+# Check median LOS less than mean LOS
+all(losA$median < losA$mean_los)
